50aa6da2ca8be591fcf78247a4c6d8868d33af81
unknown <mieraardiyantifarica@gmail.com>
2026-05-07 14:26:19 +0700

    Initial commit

---
+++ android/app/src/main/java/com/proyek3/validatornjop/MainActivity.java
@@ -0,0 +1,5 @@
+package com.proyek3.validatornjop;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.proyek3.validatornjop',
+  appName: 'Validator NJOP',
+  webDir: 'out'
+};
+
+export default config;
