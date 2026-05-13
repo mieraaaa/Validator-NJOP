659eb4a6903b067595ecfa3a02ee68c277a5c97e
unknown <mieraardiyantifarica@gmail.com>
2026-05-13 19:13:56 +0700

    Menambahkan login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Plus_Jakarta_Sans, Inter} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
-});
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const jakartaSans = Plus_Jakarta_Sans({
+  variable: "--font-plus-jakarta-sans",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${jakartaSans.variable} ${inter.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import { CircleUser } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Validator NJOP</h1>
-    </div>
+    <main className="w-full max-w-md mx-auto min-h-screen relative overflow-hidden bg-white">
+
+      {/* Header */}
+      <div className="bg-[#FAF8FF] w-full h-18.25 sticky top-0 flex items-center border border-[#C5C5D3] shadow-md">
+        <h1 className="text-[#00236F] font-bold font-[inter] text-[24px] flex-1 text-center">ValidatorNJOP</h1>
+        <CircleUser className="flex size-5 text-[#1A1B21]"/>
+      </div>
+    </main>
   );
 }
