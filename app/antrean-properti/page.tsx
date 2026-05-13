d6f641ca650f30aa571c93c85ff83d4cc40291f0
unknown <mieraardiyantifarica@gmail.com>
2026-05-14 01:10:38 +0700

    Menambah sisa card properti dan footer

--- app/antrean-properti/page.tsx
+++ app/antrean-properti/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import { CircleUser, Search, CircleAlert, Clock, MapPin, ArrowRight } from 'lucide-react';
+import { CircleUser, Search, CircleAlert, Clock, MapPin, ArrowRight, History, User } from 'lucide-react';
 
 export default function Home() {
   return (
-    <main className="w-full max-w-md mx-auto min-h-screen relative overflow-hidden bg-[#f8fafc]">
+    <main className="w-full max-w-md mx-auto min-h-screen relative overflow-hidden bg-[#f8fafc] pb-20">
 
       {/* Header */}
       <header className="bg-[#FAF8FF] w-full h-[73px] sticky top-0 flex justify-end items-center border border-[#C5C5D3] shadow-xs">
@@ -40,7 +40,7 @@ export default function Home() {
             />
         </div>
         {/* Filter */}
-        <div className="w-full mx-auto mt-3 flex justify-start items-center gap-3">
+        <div className="w-full mx-auto mt-3 flex justify-start items-center gap-3 mb-5">
             <button type="button"
                 className="bg-[#002452] text-white text-[16px] py-3 px-3 rounded-xl flex justify-center items-center gap-2">
                     <CircleAlert className="size-4 text-white"/>
@@ -55,7 +55,7 @@ export default function Home() {
 
         {/* List Properti */}
         {/* Card 1 */}
-        <div className="w-full bg-white border-2 border-[#C5C5D3] rounded-md mt-5 py-4 px-4 shadow-xs">
+        <div className="w-full bg-white border-2 border-[#C5C5D3] rounded-md mb-2 py-4 px-4 shadow-xs">
             <div className="flex gap-4">
                 <Image
                     src="/images/antrean-properti/rumah-1.svg"
@@ -96,7 +96,114 @@ export default function Home() {
                 </button>
             </div>
         </div>
+
+        {/* Card 2 */}
+        <div className="w-full bg-white border-2 border-[#C5C5D3] rounded-md mb-2 py-4 px-4 shadow-xs">
+            <div className="flex gap-4">
+                <Image
+                    src="/images/antrean-properti/rumah-2.svg"
+                    alt="Foto Rumah 2"
+                    width={78}
+                    height={78}
+                    className="shrink-0 object-cover rounded-md"
+                />
+                <div className="w-full flex justify-between items-start gap-2">
+                    <div className="pt-3 flex flex-col min-w-0">
+                        <h3 className="font-bold text-[12px] text-[#444651]">NOP</h3>
+                        <p className="font-bold text-[14px] text-[#00236F]">32.73.010.005.011-0</p>
+                        <div className="flex items-center gap-1 mt-1">
+                            <MapPin className="size-4 shrink-0 text-[#444651]"/>
+                            <p className="text-[14px] text-[#1A1B21] leading-tight line-clamp-1">Komplek Permata B3</p>
+                        </div>
+                    </div>
+                    <div className="bg-[#FEF3C7] border border-[#FCD34D] text-[#92400E] font-bold text-[10px] rounded-2xl py-1 px-2 whitespace-nowrap shrink-0">
+                        Prioritas Sedang
+                    </div>
+                </div>
+            </div>
+            <hr className="border-[#E3E1E9] mt-4 mb-2 border-t-2"></hr>
+            <div className="w-full flex justify-between items-center gap-2">
+                <div className="text-[12px] flex flex-wrap items-center gap-x-3 gap-y-1">
+                    <div className="flex gap-1 text-[#1A1B21]">
+                        <span className="font-bold">Status:</span>
+                        Baru
+                    </div>
+                    <span className="text-[#757682] text-[15px] hidden sm:block">•</span>
+                    <div className="flex gap-1 text-[#1A1B21]">
+                        <span className="font-bold">Deadline:</span>
+                        7 Hari Lagi
+                    </div>
+                </div>
+                <button type="button" className="shrink-0">
+                    <ArrowRight className="size-5 text-[#444651]"/>
+                </button>
+            </div>
+        </div>
+
+        {/* Card 3 */}
+        <div className="w-full bg-white border-2 border-[#C5C5D3] rounded-md mb-2 py-4 px-4 shadow-xs">
+            <div className="flex gap-4">
+                <Image
+                    src="/images/antrean-properti/rumah-3.svg"
+                    alt="Foto Rumah 3"
+                    width={78}
+                    height={78}
+                    className="shrink-0 object-cover rounded-md"
+                />
+                <div className="w-full flex justify-between items-start gap-2">
+                    <div className="pt-3 flex flex-col min-w-0">
+                        <h3 className="font-bold text-[12px] text-[#444651]">NOP</h3>
+                        <p className="font-bold text-[14px] text-[#00236F]">32.73.010.008.022-0</p>
+                        <div className="flex items-center gap-1 mt-1">
+                            <MapPin className="size-4 shrink-0 text-[#444651]"/>
+                            <p className="text-[14px] text-[#1A1B21] leading-tight line-clamp-1">Jl. Gatot Subroto</p>
+                        </div>
+                    </div>
+                    <div className="bg-[#E3E1E9] border border-[#C5C5D3] text-[#1A1B21] font-bold text-[10px] rounded-2xl py-1 px-2 whitespace-nowrap shrink-0">
+                        Prioritas Rendah
+                    </div>
+                </div>
+            </div>
+            <hr className="border-[#E3E1E9] mt-4 mb-2 border-t-2"></hr>
+            <div className="w-full flex justify-between items-center gap-2">
+                <div className="text-[12px] flex flex-wrap items-center gap-x-3 gap-y-1">
+                    <div className="flex gap-1 text-[#1A1B21]">
+                        <span className="font-bold">Status:</span>
+                        Baru
+                    </div>
+                    <span className="text-[#757682] text-[15px] hidden sm:block">•</span>
+                    <div className="flex gap-1 text-[#1A1B21]">
+                        <span className="font-bold">Deadline:</span>
+                        7 Hari Lagi
+                    </div>
+                </div>
+                <button type="button" className="shrink-0">
+                    <ArrowRight className="size-5 text-[#444651]"/>
+                </button>
+            </div>
+        </div>
       </div>
+      {/* Footer */}
+      <footer className="w-full max-w-md bg-[#EEEDF4] border-t border-[#C5C5D3] flex justify-around items-center py-2 mt-10 fixed bottom-0 left-0 z-50 left-1/2 -translate-x-1/2">
+        <div className="bg-[#1E3A8A] rounded-lg flex flex-col justify-center items-center font-bold text-[12px] text-[#90A8FF] py-1 px-4 shrink-0 ">
+            <Image
+                src="/images/common/navbar-queue.svg"
+                alt="Logo Navbar Antrian"
+                width={18}
+                height={20}
+                className="shrink-0 mb-1"
+            />
+            Queue
+        </div>
+        <div className="flex flex-col justify-center items-center font-bold text-[12px] text-[#444651] py-1 px-4 shrink-0">
+            <History className="size-5 text-[#444651] mb-1"/>
+            History
+        </div>
+        <div className="flex flex-col justify-center items-center font-bold text-[12px] text-[#444651] py-1 px-4">
+            <User className="size-5 text-[#444651] mb-1"/>
+            Profile
+        </div>
+      </footer>
     </main>
   );
 }
