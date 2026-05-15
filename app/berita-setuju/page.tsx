186cd32cde348dbbe944b8ea81649e47949edd75
unknown <mieraardiyantifarica@gmail.com>
2026-05-15 08:43:27 +0700

    Menyelesaikan sisa fitur halaman

--- app/berita-setuju/page.tsx
+++ app/berita-setuju/page.tsx
@@ -4,13 +4,13 @@ import { ArrowLeft, Download, CircleCheck } from 'lucide-react';
 
 export default function Home() {
   return (
-    <main className="w-full max-w-md mx-auto min-h-screen relative overflow-x-hidden bg-[#f8fafc] pb-20">
+    <main className="w-full max-w-md mx-auto min-h-screen relative overflow-x-hidden bg-[#f8fafc] pb-5">
 
       {/* Header */}
       <header className="bg-[#FAF8FF] w-full h-[57px] sticky top-0 z-50 flex justify-between items-center border-b border-[#C5C5D3] shadow-xs pl-6 pr-3">
         <Link href="/validasi-setuju" className="w-full flex justify-start items-center text-[#00236F] gap-1">
             <ArrowLeft className="flex size-5 shrink-0"/>
-            <h1 className="font-mono font-semibold text-[#16px]">Kembali</h1>
+            <h1 className="font-mono font-semibold text-[16px]">Kembali</h1>
         </Link>
         <button type="button">
             <Download className="flex size-5 shrink-0 text-[#757682]"/>
@@ -33,9 +33,9 @@ export default function Home() {
             </h2>
             <span className="font-public-sans text-[14px] text-[#444651]">Nomor: BA/123/X/2023</span>
         </div>
-        <hr className="border-[#1A1B21] w-full mx-auto border-t-3"/>
+        <hr className="border-[#1A1B21] w-full mx-auto border-t-2"/>
         {/* Data Objek Pajak */}
-        <div className="w-full border-l-5 border-l-[#00236F] text-[#00236F] px-1">
+        <div className="w-full border-l-4 border-l-[#00236F] text-[#00236F] px-1">
             <h3 className="font-mono font-bold text-[11px]">DATA OBJEK PAJAK</h3>
         </div>
         <div className="w-full border border-[#C5C5D3] bg-[#FAF8FF] flex flex-col gap-5 items-center py-4 px-4">
@@ -57,7 +57,7 @@ export default function Home() {
             </div>
         </div>
         {/* Hasil Validasi NJOP */}
-        <div className="w-full border-l-5 border-l-[#00236F] text-[#00236F] px-1">
+        <div className="w-full border-l-4 border-l-[#00236F] text-[#00236F] px-1">
             <h3 className="font-mono font-bold text-[11px]">HASIL VALIDASI NJOP</h3>
         </div>
         <div className="w-full bg-[#DCE1FF] border border-[#B6C4FF] rounded-md flex flex-col justify-center items-center gap-4 px-5 py-5 text-center">
@@ -74,6 +74,36 @@ export default function Home() {
                     Berdasarkan hasil peninjauan lapangan pada tanggal 24 Oktober 2023, data fisik dan nilai 
                     bangunan telah sesuai dengan kondisi faktual.
                 </p>
+            </div>
+        </div>
+        <hr className="border-[#C5C5D3] w-full mx-auto border-t mt-8"/>
+        {/* Bagian Tanda Tangan */}
+        <div className="w-full flex justify-between items-end gap-3 text-center text-[#1A1B21]">
+            <div className="flex flex-col items-center px-2 flex-1 min-w-0">
+                <h5 className="font-public-sans text-[12px] ">Mengetahui, Supervisor Pemeriksa</h5>
+                <Image
+                    src="/images/common/ttd-supervisor.svg"
+                    alt="Logo Tanda Tangan Supervisor"
+                    width={58}
+                    height={56}
+                    className="shrink-0 py-5"        
+                />
+                <hr className="border-[#444651] w-[70%] mx-auto border-t pb-1"/>
+                <span className="font-mono font-semibold text-[16px]">Budi Santoso, S.IP</span>
+                <span className="font-mono font-medium text-[14px] text-[#444651]">NIP. 198012012005011002</span>
+            </div>
+            <div className="flex flex-col items-center px-2 flex-1 min-w-0">
+                <h5 className="font-public-sans text-[12px] ">Petugas, Penilai Pajak Lapangan</h5>
+                <Image
+                    src="/images/common/ttd-petugas.svg"
+                    alt="Logo Tanda Tangan Petugas"
+                    width={51}
+                    height={48}
+                    className="shrink-0 pb-5 pt-7"        
+                />
+                <hr className="border-[#444651] w-[70%] mx-auto border-t pb-1"/>
+                <span className="font-mono font-semibold text-[16px]">Ahmad Hidayat</span>
+                <span className="font-mono font-medium text-[14px] text-[#444651]">NIP. 199203152019021001</span>
             </div>
         </div>
       </div>
