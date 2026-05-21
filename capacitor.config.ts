6fb3c7db27ab4a054afedf3eac4c623d9b4b3f6b
chelseanatasja <162960783+chelseanatasja@users.noreply.github.com>
2026-05-21 13:29:49 +0700

    Mengaktifkan Bypass CORS di Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.proyek3.validatornjop',
   appName: 'Validator NJOP',
-  webDir: 'out'
+  webDir: 'out',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
